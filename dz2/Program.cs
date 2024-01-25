6d4195b81291d44e25e6455605d929d0a6c827b9
Aleksey Myakinin <amykis@mail.ru>
2024-01-25 17:15:45 +0300

    add dz2

---
+++ dz2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 2: Задайте строку, содержащую латинские буквы в обоих регистрах. Сформируйте строку, в которой все заглавные буквы замененены на строчные. 
+
+// Пример
+// “aBcD1ef!-” => “abcd1ef!-”  
+
+Console.Clear();
+
+string str = "aBcD1ef!-";
+char [] ch = new char [str.Length];
+
+for (int i = 0; i < str.Length; i++){
+    ch[i] = (str.ToLower().ToCharArray()[i]);
+}
+
+str = "";
+foreach (char e in ch){
+    str += e;
+}
+Console.Write(str);
