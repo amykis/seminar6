6379c4de41a6d66d5a36b66c562b6a85437fff2a
Aleksey Myakinin <amykis@mail.ru>
2024-01-25 21:51:05 +0300

    add dz4*

---
+++ dz4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 4 *(не обязательная): Задайте строку, состоящую из слов, разделенных пробелами. Сформировать строку,
+// в которой слова расположены в обратном порядке. В полученной строке слова должны быть также разделены пробелами.
+
+// Пример  
+// “Hello my world” => “world my Hello” 
+
+string GetReversWords(string input) {
+    string[] words = input.Split(' ');
+    Array.Reverse(words);
+    string result = "";
+    foreach(string item in words) {
+       result += $"{item}" + " ";
+    }
+    return result.Trim();
+}
+
+string input = "Hello my world";
+Console.WriteLine(GetReversWords(input));
